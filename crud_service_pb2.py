f9487ca514f7400476ae91ee39eb0feb459e7e9a
thanhbinhs <thanhbinhsmart@gmail.com>
2025-02-10 16:10:12 +0700

    final

--- crud_service_pb2.py
+++ crud_service_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12\x63rud_service.proto\"$\n\x06Record\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\"\x12\n\x03Key\x12\x0b\n\x03key\x18\x01 \x01(\t\"0\n\x08Response\x12\x13\n\x0bstatus_code\x18\x01 \x01(\x05\x12\x0f\n\x07message\x18\x02 \x01(\t\"\x07\n\x05\x45mpty2\xa8\x01\n\x0c\x43loudStorage\x12\x1e\n\x06\x43reate\x12\x07.Record\x1a\t.Response\"\x00\x12\x19\n\x04Read\x12\x04.Key\x1a\t.Response\"\x00\x12\x1e\n\x06Update\x12\x07.Record\x1a\t.Response\"\x00\x12\x1b\n\x06\x44\x65lete\x12\x04.Key\x1a\t.Response\"\x00\x12 \n\tHeartbeat\x12\x06.Empty\x1a\t.Response\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12\x63rud_service.proto\"$\n\x06Record\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\"\x12\n\x03Key\x12\x0b\n\x03key\x18\x01 \x01(\t\"0\n\x08Response\x12\x13\n\x0bstatus_code\x18\x01 \x01(\x05\x12\x0f\n\x07message\x18\x02 \x01(\t\"\x07\n\x05\x45mpty\",\n\x10SnapshotResponse\x12\x18\n\x07records\x18\x01 \x03(\x0b\x32\x07.Record2\xd1\x01\n\x0c\x43loudStorage\x12\x1e\n\x06\x43reate\x12\x07.Record\x1a\t.Response\"\x00\x12\x19\n\x04Read\x12\x04.Key\x1a\t.Response\"\x00\x12\x1e\n\x06Update\x12\x07.Record\x1a\t.Response\"\x00\x12\x1b\n\x06\x44\x65lete\x12\x04.Key\x1a\t.Response\"\x00\x12 \n\tHeartbeat\x12\x06.Empty\x1a\t.Response\"\x00\x12\'\n\x08Snapshot\x12\x06.Empty\x1a\x11.SnapshotResponse\"\x00\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -39,6 +39,8 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_RESPONSE']._serialized_end=128
   _globals['_EMPTY']._serialized_start=130
   _globals['_EMPTY']._serialized_end=137
-  _globals['_CLOUDSTORAGE']._serialized_start=140
-  _globals['_CLOUDSTORAGE']._serialized_end=308
+  _globals['_SNAPSHOTRESPONSE']._serialized_start=139
+  _globals['_SNAPSHOTRESPONSE']._serialized_end=183
+  _globals['_CLOUDSTORAGE']._serialized_start=186
+  _globals['_CLOUDSTORAGE']._serialized_end=395
 # @@protoc_insertion_point(module_scope)
--- crud_service_pb2_grpc.py
+++ crud_service_pb2_grpc.py
@@ -59,6 +59,11 @@ class CloudStorageStub(object):
                 request_serializer=crud__service__pb2.Empty.SerializeToString,
                 response_deserializer=crud__service__pb2.Response.FromString,
                 _registered_method=True)
+        self.Snapshot = channel.unary_unary(
+                '/CloudStorage/Snapshot',
+                request_serializer=crud__service__pb2.Empty.SerializeToString,
+                response_deserializer=crud__service__pb2.SnapshotResponse.FromString,
+                _registered_method=True)
 
 
 class CloudStorageServicer(object):
@@ -94,6 +99,12 @@ class CloudStorageServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def Snapshot(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_CloudStorageServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -121,6 +132,11 @@ def add_CloudStorageServicer_to_server(servicer, server):
                     servicer.Heartbeat,
                     request_deserializer=crud__service__pb2.Empty.FromString,
                     response_serializer=crud__service__pb2.Response.SerializeToString,
+            ),
+            'Snapshot': grpc.unary_unary_rpc_method_handler(
+                    servicer.Snapshot,
+                    request_deserializer=crud__service__pb2.Empty.FromString,
+                    response_serializer=crud__service__pb2.SnapshotResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -258,6 +274,33 @@ class CloudStorage(object):
             '/CloudStorage/Heartbeat',
             crud__service__pb2.Empty.SerializeToString,
             crud__service__pb2.Response.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def Snapshot(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/CloudStorage/Snapshot',
+            crud__service__pb2.Empty.SerializeToString,
+            crud__service__pb2.SnapshotResponse.FromString,
             options,
             channel_credentials,
             insecure,
